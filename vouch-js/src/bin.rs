c34463a84ce7d69531aebf5d790853a97be4946d
rndhouse <rndhouse@protonmail.com>
2021-09-08 12:29:18 +0100

    Split vouch-js into its own repo.

--- vouch-js/src/bin.rs
+++
@@ -1,7 +0,0 @@
-use vouch_js_lib;
-use vouch_lib::extension::FromLib;
-
-fn main() {
-    let mut extension = vouch_js_lib::JsExtension::new();
-    vouch_lib::extension::commands::run(&mut extension).unwrap();
-}
